99db7c06e9476f1d42cdc6239c7851a6aa7a71a5
Angel Grablev <agrublev@gmail.com>
2019-05-28 08:52:12 -0700

    Fixed

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-import React from "react"+;
+import React from "react";
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-    console.warn("z",zesd);
 ReactDOM.render(<App />, document.getElementById('root'));
 
 if (module.hot) {
