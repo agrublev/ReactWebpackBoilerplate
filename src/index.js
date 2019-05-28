57966274944fed5d1a5eb461e1bb2697d4c1fd7f
Angel Grablev <agrublev@gmail.com>
2019-05-28 08:55:31 -0700

    BR

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+console.log("DOESNT EX",NOEX IST);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
