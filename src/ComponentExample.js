876714f2c6624fe4ce478f1daed48efa3e8e3dda
Angel Grablev <agrublev@gmail.com>
2019-08-14 15:25:21 +0300

    Update ComponentExample.js

--- src/ComponentExample.js
+++ src/ComponentExample.js
@@ -2,11 +2,11 @@ import React, { Component } from "react";
 
 /** ADDING BAD CODE FOR TESTING */
 
-const treeBBA = 'Tesfragdt 53'+"SD"+'sadasd' + "SADA" + 'SAD';
+const treeBBA = 'Tesfratgdt 53'+"SD"+'sadasd' + "SADA" + 'SAD';
 const z = "TElll";
 
-const treeA = 'Tesdt 53';
-const z = "TEllZl";
+const treeA = 'Tesdttt 53';
+const z = "TEllttZl";
 
 
 class ComponentExample extends Component {
