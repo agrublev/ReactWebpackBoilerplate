ec3c020fee1218c41a0180eecd4980e40fd40587
Angel Grablev <agrublev@gmail.com>
2019-10-02 00:40:44 -0700

    Update ComponentExample.js

--- src/ComponentExample.js
+++ src/ComponentExample.js
@@ -4,6 +4,7 @@ import React, { Component } from "react";
 
 const treeBBA = 'Tesfratgdt 53'+"SD"+'sadasd' + "SADA" + 'SAD';
 const z = "TElll";
+const z = tttt;
 
 const treeA = 'Tesdttt 53';
 const z = "TEllttZl";
@@ -16,8 +17,6 @@ class ComponentExample extends Component {
             This an example Component, because examples.
             
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eleifend feugiat ipsum et elementum. Aenean sapien felis, porta et consequat quis, interdum at purus. Etiam tempor tortor sem, in auctor velit aliquam vitae. Sed mattis mollis eros lacinia porta. Praesent id scelerisque libero, vel mollis eros. Nunc dictum, risus nec aliquet facilisis, tortor orci mollis tortor, eu lobortis mauris tellus ac diam. Proin elementum posuere nunc, ac accumsan lectus feugiat vitae. Pellentesque sagittis finibus sem, et aliquam nisi finibus vitae.
-
-Vestibulum auctor ligula ut tempus pretium. Nunc maximus lectus nec purus finibus, feugiat molestie nunc vestibulum. Fusce lorem sapien, rutrum a porta at, sollicitudin nec orci. Integer in ligula viverra, scelerisque metus id, viverra tortor. Cras lacus felis, commodo eu velit ornare, pulvinar aliquam arcu. Vestibulum eu lorem nec diam dignissim interdum et in est. Praesent eget tempor nunc. In congue neque et risus dapibus eleifend. Cras at nisl mollis, ullamcorper arcu at, posuere justo. Praesent consectetur vel mauris vitae posuere. Morbi porta iaculis velit id condimentum. Pellentesque ut elit quis sem tincidunt volutpat. Quisque accumsan ultrices nunc vel ultrices. Proin vulputate ligula mauris, eget pulvinar purus rhoncus vitae. Pellentesque interdum neque vel ex pharetra, sed aliquet velit pretium. Phasellus varius est risus, eget lacinia ex tincidunt id.
 
 Vivamus eu massa dolor. Nullam porta ante ultrices ante egestas vehicula. Curabitur lobortis tellus auctor vehicula pharetra. Nunc vestibulum et dolor vitae varius. Maecenas eleifend feugiat neque, at scelerisque sapien imperdiet in. Integer lacinia vel ante vitae aliquam. Cras nec gravida odio. Proin congue orci non auctor imperdiet. Donec consectetur est ante, sit amet sodales libero lacinia vel. Suspendisse eget orci id ex facilisis eleifend in nec quam. Duis sollicitudin ultricies interdum. Phasellus suscipit enim augue. Integer hendrerit efficitur porttitor. Maecenas id libero nec nisl tincidunt facilisis non a risus. Curabitur vestibulum sagittis mauris id molestie.
 
