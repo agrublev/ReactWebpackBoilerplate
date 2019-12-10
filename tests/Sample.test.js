f96351896221375d280ee6bace7dfe0588b33587
agrublev <agrublev@gmail.com>
2019-12-10 04:31:42 -0800

    fail

--- tests/Sample.test.js
+++ tests/Sample.test.js
@@ -1,6 +1,6 @@
 describe("Test suite 1", () => {
     test("Check 1 === 1", async () => {
-        expect(1).toBe(1);
+        expect(1).toBe(2);
     });
     test("Check 3 second wait", async () => {
         await new Promise(resolve => setTimeout(resolve,3000));
